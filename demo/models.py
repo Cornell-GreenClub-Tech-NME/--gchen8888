ba78f73d1de1ef23d098a7e19d5d665fa45ab916
GC <gc@dhcp-vl2041-8035.eduroam.cornell.edu>
2024-12-03 18:54:11 -0500

    try the todos

--- demo/models.py
+++ demo/models.py
@@ -7,9 +7,18 @@ class User(models.Model):
     name = models.TextField()
     username = models.TextField()
     balance = models.FloatField()
-    # TODO: Create relationship to Transaction model
+   
 
     def __str__(self):
         return self.name
     
 # TODO: Create Transaction Model
+class Transaction(models.Model):
+    id = models.TextField(primary_key=True) 
+    amount = models.FloatField()
+    senderid = models.ForeignKey(User, on_delete=models.CASCADE, related_name='senderid')
+    receiverid = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiverid')
+    pending = models.BooleanField()
+
+     # TODO: Create relationship to Transaction model
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
